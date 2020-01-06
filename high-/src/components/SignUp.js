211190ad50ed5427bce54f9083810f135fbe2f8f
Amber Mohler <ambermohler@imac.lan>
2020-01-06 18:38:54 -0500

    added styled components to form

--- high-/src/components/SignUp.js
+++ high-/src/components/SignUp.js
@@ -5,45 +5,91 @@ import twitter from '../twitter-connect.svg';
 import google from '../google-connect.svg';
 import styled from 'styled-components';
 
-
-const Header = styled.div`
-    display: flex;
-    font-size: 1.5rem;
+const MainDiv = styled.div`
+    font-family: 'Roboto', sans-serif;
+    width: 420px;
+    height: 689px;
+    margin: auto;
+    border: 1px solid #D7D7D7;
+    border-radius: 4px;
+    margin-top: 15%;
+    
 `;
 
+
+const HeaderCont = styled.div`
+    display: flex;
+    margin: auto;
+    justify-content: center;
+`;
+
+const Header = styled.h1`
+    font-size: 25px;
+    font-weight: bold;
+    line-height: 29px
+    margin-top: 31.25px;
+    padding-right: 12.92px;
+`;
+
+const LogoImg = styled.img`
+    width: 32.27px;
+    height: 36.47px;
+    margin-top: 21.76px;
+`;
+
+const Label = styled.label`
+font-size: 16px;
+line-height: 19px;
+margin-left: 16px;
+
+`;
+
+const Input = styled.input`
+width: 378px;
+height: 44px;
+margin-top: 4px;
+margin-left: 16px;
+margin-bottom: 24px;
+border: 1px solid #ACACAC
+background-color: #F7F7F7
+padding-left: 10px;
+font-size: 12px;
+
+
+`;
 
 
 export default function SignUp() {
     return (
-        <div>
-            <Header>
-            <h1>Med Cabinet</h1>
-            <img src={logo} className="App-logo" alt="logo" />
-            </Header>
+        <MainDiv>
+            <HeaderCont>
+            <Header>Med Cabinet</Header>
+            <LogoImg src={logo} alt="logo" />
+            </HeaderCont>
         <form>
-            <label htmlFor='email'>Email</label><br />
-            <input 
+            <Label htmlFor='email'>Email</Label><br />
+            <Input 
             id='email'
             type='text'
             name='email'
             placeholder='example@example.com'
-            size='40'
+            
             /><br />
-            <label htmlFor='username'>Username</label><br />
-            <input
+            <Label htmlFor='username'>Username</Label><br />
+            <Input
             id='username'
             type='text'
             name='username'
             placeholder='Create your username'
-            size='40'
+            
             /><br />
-            <label htmlFor='password'>Password</label><br />
-            <input
+            <Label htmlFor='password'>Password</Label><br />
+            <Input
             id='password'
             type='password'
             name='password'
             placeholder='8 or more characters'
-            size='40'
+            
             />
             <p>We respect privacy. Names and emails are not displayed publically. Nothing will be posted to your facebook, google or twitter account without your permission. By creating your Med Cabinet account, you agree to the terms of use and privacy policy.
             </p>
@@ -58,6 +104,6 @@ export default function SignUp() {
         </form>
 
 
-        </div>
+        </MainDiv>
     )
 }
