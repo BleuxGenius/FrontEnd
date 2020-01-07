b62c8cb4719ca03c072e16b39c26448085c3e712
Dani Blackwell <bleuxgenius@gmail.com>
2020-01-07 10:54:09 -0600

    refactored

--- high-/src/App.js
+++ high-/src/App.js
@@ -4,7 +4,7 @@ import Login from './components/login.js';
 import { Button, Form, FormGroup, Label, Input} from 'reactstrap';
 import logo from './components/images/Logo.png';
 import {FacebookLoginButton, TwitterLoginButton, GoogleLoginButton} from 'react-social-login-buttons';
-import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn, MDBCard, MDBCardBody } from 'mdbreact';
+import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody } from 'mdbreact';
 
 
 const saveLogin = [
@@ -15,92 +15,55 @@ const saveLogin = [
 function App() {
   return (
 
-    <MDBContainer>
-      <MDBRow>
-        <MDBCol md="6">
-          <MDBCard>
-            <MDBCardBody>
-              <form>
-                <p className="h4 text-center py-4">Sign up</p>
-                <div className="grey-text">
-                  <MDBInput
-                    label="Your name"
-                    icon="user"
-                    group
-                    type="text"
-                    validate
-                    error="wrong"
-                    success="right"
-                  />
-                  <MDBInput
-                    label="Your email"
-                    icon="envelope"
-                    group
-                    type="email"
-                    validate
-                    error="wrong"
-                    success="right"
-                  />
-                  <MDBInput
-                    label="Confirm your email"
-                    icon="exclamation-triangle"
-                    group
-                    type="text"
-                    validate
-                    error="wrong"
-                    success="right"
-                  />
-                  <MDBInput
-                    label="Your password"
-                    icon="lock"
-                    group
-                    type="password"
-                    validate
-                  />
-                </div>
-                <div className="text-center py-4 mt-3">
-                  <MDBBtn color="cyan" type="submit">
-                    Register
-                  </MDBBtn>
-                </div>
-              </form>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-      </MDBRow>
+  <MDBContainer className="container d-flex flex-column w-50 justify-content-center" >
+    <MDBRow className="h-30px">
+      <MDBCol md="11">
+         <MDBCard>
+           <MDBCardBody>
+    <Form className="login-form">
+      <p>Med Cabinet </p>
+      <img src={logo} className="Logo" alt="logo"/> 
+      <h2 className="text-center">Login</h2>
+      <FormGroup>
+        <Label >Email</Label>
+        <Input type="email" placeholder="Email"/>
+        </FormGroup>
+        <FormGroup>
+        <Label>Password</Label>
+        <Input type="password" placeholder="Password"/>
+        </FormGroup>
+        <button className="btn-sm btn-dark-block">Sign In</button>
+
+    <div class="d-flex justify-content-between">
+        <div>
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="defaultLoginFormRemember"/>
+                <label class="custom-control-label" for="defaultLoginFormRemember">Remember me</label>
+           </div>
+        </div>
+        <div>
+            <a href="">Forgot password?</a>
+        </div>
+    </div>
+
+          <div className="text-center pt-3">
+            Or
+          </div>
+          <FacebookLoginButton/>
+          <GoogleLoginButton/>
+          <TwitterLoginButton/>
+            <p>
+              Don't have an account?<a href="/" class="signUpLink">Sign Up</a>
+            </p>
+    </Form>
+    </MDBCardBody>
+    </MDBCard>
+    </MDBCol>
+  </MDBRow>
     </MDBContainer>
-  );
-
-
-    // <Form className="login-form">
-    //   <h2>MedCabinet</h2>
-    //   <img src={logo} className="Logo" alt="logo"/>
-    //   <h2 className="text-center">Login</h2>
-    //   <FormGroup>
-    //     <Label >Email</Label>
-    //     <Input type="email" placeholder="Email"/>
-    //     </FormGroup>
-    //     <FormGroup>
-    //     <Label>Password</Label>
-    //     <Input type="password" placeholder="Password"/>
-    //     </FormGroup>
-    //     <button className="btn-sm btn-dark-block">Sign In</button>
-    //       <div className="text-center pt-3">
-    //         Or
-    //       </div>
-    //       <FacebookLoginButton/>
-    //       <GoogleLoginButton/>
-    //       <TwitterLoginButton/>
-    //         <p>
-    //           Don't have an account?<a href="/" class="signUpLink">Sign Up</a>
-    //         </p>
-           
-            
-
-    // </Form>
 
     
-  // );
+  );
 }
 
 export default App;
