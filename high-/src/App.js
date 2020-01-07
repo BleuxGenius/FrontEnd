783cd65502e1977a1f8c1139961eb55e07ce8dab
Dani Blackwell <bleuxgenius@gmail.com>
2020-01-07 12:38:55 -0600

    moved over files
    added opn submit and a bit of funtionality.

--- high-/src/App.js
+++ high-/src/App.js
@@ -1,69 +1,18 @@
 import React from "react";
 import "./App.css";
 import Login from './components/login.js';
-import { Button, Form, FormGroup, Label, Input} from 'reactstrap';
-import logo from './components/images/Logo.png';
-import {FacebookLoginButton, TwitterLoginButton, GoogleLoginButton} from 'react-social-login-buttons';
-import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody } from 'mdbreact';
 
 
-const saveLogin = [
-  'keepMeLoggedIn',
-  'dontKeepMeLoggedIn'
-];
+
+
 
 function App() {
+
+  
   return (
 
-  <MDBContainer className="container d-flex flex-column w-50 justify-content-center" >
-    <MDBRow className="h-30px">
-      <MDBCol md="11">
-         <MDBCard>
-           <MDBCardBody>
-    <Form className="login-form">
-      <p>Med Cabinet </p>
-      <img src={logo} className="Logo" alt="logo"/> 
-      <h2 className="text-center">Login</h2>
-      <FormGroup>
-        <Label >Email</Label>
-        <Input type="email" placeholder="Email"/>
-        </FormGroup>
-        <FormGroup>
-        <Label>Password</Label>
-        <Input type="password" placeholder="Password"/>
-        </FormGroup>
-        <button className="btn-sm btn-dark-block">Sign In</button>
-
-    <div class="d-flex justify-content-between">
-        <div>
-            <div class="custom-control custom-checkbox">
-                <input type="checkbox" class="custom-control-input" id="defaultLoginFormRemember"/>
-                <label class="custom-control-label" for="defaultLoginFormRemember">Remember me</label>
-           </div>
-        </div>
-        <div>
-            <a href="">Forgot password?</a>
-        </div>
-    </div>
-
-          <div className="text-center pt-3">
-            Or
-          </div>
-          <FacebookLoginButton/>
-          <GoogleLoginButton/>
-          <TwitterLoginButton/>
-            <p>
-              Don't have an account?<a href="/" class="signUpLink">Sign Up</a>
-            </p>
-    </Form>
-    </MDBCardBody>
-    </MDBCard>
-    </MDBCol>
-  </MDBRow>
-    </MDBContainer>
-
-    
+  <Login/>
   );
-}
+};
 
 export default App;
--- high-/src/components/login.js
+++ high-/src/components/login.js
@@ -1,12 +1,80 @@
 import React from "react";
+import { Button, Form, FormGroup, Label, Input} from 'reactstrap';
+import logo from './images/Logo.png';
+import {FacebookLoginButton, TwitterLoginButton, GoogleLoginButton} from 'react-social-login-buttons';
+import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody } from 'mdbreact';
+import { useState } from "react";
 
 
- class Login extends React.Component {
-  render() {
+
+ export default function Login(props) {
+  
+    const[signIn, setSignIn] = useState({
+      email:"", password:"" 
+    });
+  
+    const Login = e => {
+      e.preventDefault();
+      props.logIn(signIn)
+      }
+  
+      const handleChanges = e => {
+        setSignIn({
+          ...signIn, [e.target.name]: e.target.value
+        });
+      };
+  
     return(
-      <h1>Login</h1>
-    )
-  }
-}
+     
+  <MDBContainer className="container flex-row w-50 justify-content-center" >
+  <MDBRow>
+    <MDBCol md="11">
+       <MDBCard>
+         <MDBCardBody>
+  <Form className="login-form" onSubmit={Login}>
+    <p>Med Cabinet </p>
+    <MDBCol>
+    <img src={logo} className="imageThumbnail" alt="logo"/> 
+    </MDBCol>
+    <h2 className="text-center">Login</h2>
+    <FormGroup>
+      <Label >Email</Label>
+      <Input type="email" name="email" placeholder="Email" value={signIn.email} onChange={handleChanges}/>
+      </FormGroup>
+      <FormGroup>
+      <Label>Password</Label>
+      <Input type="password" name="password" placeholder="Password" value={signIn.password} onChange={handleChanges}/>
+      </FormGroup>
+      <button className="btn-sm btn-dark-block">Sign In</button>
 
-export default Login;
+  <div class="d-flex justify-content-between">
+      <div>
+          <div class="custom-control custom-checkbox">
+              <input type="checkbox" class="custom-control-input" id="defaultLoginFormRemember"/>
+              <label class="custom-control-label" for="defaultLoginFormRemember">Remember me</label>
+         </div>
+      </div>
+      <div>
+          <a href="">Forgot password?</a>
+      </div>
+  </div>
+
+        <div className="text-center pt-3">
+          Or
+        </div>
+        <FacebookLoginButton/>
+        <GoogleLoginButton/>
+        <TwitterLoginButton/>
+          <p>
+            Don't have an account?<a href="/" class="signUpLink">Sign Up</a>
+          </p>
+  </Form>
+  </MDBCardBody>
+  </MDBCard>
+  </MDBCol>
+</MDBRow>
+  </MDBContainer>
+
+  
+);
+};
