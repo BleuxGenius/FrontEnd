32e21492cfb13699a31e2a420d313eeb9744d6da
Anthony Lopez <anthonybeconscious@gmail.com>
2020-01-07 09:10:57 -0600

    catchin up

--- high-/src/App.js
+++ high-/src/App.js
@@ -1,8 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-
-=======
->>>>>>> 9b616eaa0edd61c21208b5891bccd7b1f352ad25
 import "./App.css";
 import SignUp from './components/SignUp'
 
@@ -10,11 +6,7 @@ import SignUp from './components/SignUp'
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-  
-=======
       <SignUp />
->>>>>>> 9b616eaa0edd61c21208b5891bccd7b1f352ad25
     </div>
   );
 }
