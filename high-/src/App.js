6d0da3ce8352cf5cb47509fcfeb6fcedb7b8000e
Anthony Lopez <anthonybeconscious@gmail.com>
2020-01-09 19:57:35 -0600

    doing things

--- high-/src/App.js
+++ high-/src/App.js
@@ -1,16 +1,31 @@
 import React from "react";
+import { Route } from 'react-router-dom';
 import "./App.css";
-import SignUp from './components/SignUp'
-import axios from "axios"
-// import { BrowserRouter } as Router from "react-router"
-import Login from "./components/Login"
+//import SignUp from './components/SignUp';
+//import Login from './components/Login';
+import NavBar from './components/NavBar'
+import StrainsCard from './components/StrainsCard';
+
 
 
 function App() {
   return (
     <div className="App">
-      {/* <SignUp /> */}
-      <Login />
+      {/* <Route exact path='/'>
+      <NavBar />
+      </Route>
+      <Route path='/signup'>
+        <SignUp />
+      </Route>
+      <Route path='/login'>
+        <Login />
+      </Route> */}
+      <NavBar />
+      <StrainsCard />
+      
+      
+      
+
 
     </div>
   );
